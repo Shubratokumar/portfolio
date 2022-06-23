294cce454480fef9ac063ac138b8a668bb2cf3b2
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-06-24 00:16:21 +0600

    banner amimation done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,38 +14,57 @@ const Banner = () => {
     words: ['Web Developer', 'Frontend Developer', 'Jr. MERN Stack Developer', 'Tech Enthusiastic .'],
     loop: 0
   })
+
   return (
-    <div>
-      
+    <div>      
       <div className="flex flex-col justify-center items-center">
-        <img src={profile} className="mask mask-hexagon-2 w-80" alt="" />
-        <div className="my-10 text-center px-10">
-        <p className='text-2xl font-light'>Hi, my name is </p>
-          <h1 className='font-bold text-4xl lg:text-5xl pb-4'><span className="text-pink-700">Shubrato Kumar Gharami .</span></h1>
-          <p className='text-2xl font-light'>I'm a </p>
-          <h1 className="font-bold text-4xl lg:text-5xl pb-4">
-            <span className="text-secondary">
-              {text}
-              <Cursor />
-            </span>
-              {/* <span className="text-secondary">
-              <Typewriter
-                words={['Web Developer', 'Frontend Developer', 'Jr. MERN Stack Developer', 'Tech Enthusiastic']}
-                loop={5}
-                cursor
-                cursorStyle='|'
-                typeSpeed={70}
-                deleteSpeed={40}
-                delaySpeed={500}
-              />
-            </span> */}            
-          </h1>
-          <p className="font-light text-2xl">
-            I love what I do. I enjoy making simple things beautiful by Coding
-            and Developing.
-          </p>
+        <div data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+         >
+          <img src={profile} className="mask mask-hexagon-2 w-80" alt="" />
         </div>
-        <div className="flex justify-center items-center ">
+        <div className="my-10 text-center px-10">
+          <div  data-aos="fade-up-right"
+          data-aos-easing="ease-in-out"
+          >
+            <p className='text-2xl font-light'>Hi, my name is </p>
+            <h1 className='font-bold text-4xl lg:text-5xl pb-4'><span className="text-pink-700">Shubrato Kumar Gharami .</span></h1>
+          </div>
+          <div data-aos="fade-up-left"
+          data-aos-easing="ease-in-out"
+          >
+            <p className='text-2xl font-light'>I'm a </p>
+            <h1 className="font-bold text-4xl lg:text-5xl pb-4">
+              <span className="text-secondary">
+                {text}
+                <Cursor />
+              </span>
+                {/* <span className="text-secondary">
+                <Typewriter
+                  words={['Web Developer', 'Frontend Developer', 'Jr. MERN Stack Developer', 'Tech Enthusiastic']}
+                  loop={5}
+                  cursor
+                  cursorStyle='|'
+                  typeSpeed={70}
+                  deleteSpeed={40}
+                  delaySpeed={500}
+                />
+              </span> */}            
+            </h1>
+            <p className="font-light text-2xl">
+              I love what I do. I enjoy making simple things beautiful by Coding
+              and Developing.
+            </p>
+          </div>
+        </div>
+        <div  data-aos="fade-up"
+        data-aos-delay="100"
+        data-aos-duration="1500"
+        data-aos-easing="ease-in-out"
+         className="flex justify-center items-center">
           <a
             className="text-2xl p-3 hover:text-secondary"
             href="https://github.com/Shubratokumar"
@@ -79,7 +98,11 @@ const Banner = () => {
             <BsFacebook></BsFacebook>
           </a>
         </div>
-        <div className='flex justify-center items-center mt-3'>
+        <div data-aos="zoom-out-up" 
+        data-aos-delay="100"
+        data-aos-duration="1500"
+        data-aos-easing="ease-in-out"
+        className='flex justify-center items-center mt-3'>
             <img src={banner} className="w-80 lg:w-6/12" alt="" />
         </div>
       </div>
